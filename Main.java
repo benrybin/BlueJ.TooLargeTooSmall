eab6cbf5b66ab86b6c56f2cd4515c016c4d171a8
Ben Rybinski <benrybinski@zipcoders-MacBook-Pro.local>
2019-05-21 17:59:35 -0400

    Done

--- Main.java
+++ Main.java
@@ -5,10 +5,12 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Scanner;
+import java.util.Random;
 public class Main
 {
     // instance variables - replace the example below with your own
-    private int x;
+    
 
     /**
      * Constructor for objects of class Main
@@ -16,7 +18,35 @@ public class Main
     public Main()
     {
         // initialise instance variables
-        x = 0;
+        
+        int randomNum;
+        int guessNum = 1;
+        int lastGuess;
+        Random rand = new Random();
+        randomNum = rand.nextInt(19) + 1;
+        System.out.println("Enter a Guess answer can be any number between 1 & 20");
+        Scanner userGuess = new Scanner(System.in);
+        
+        lastGuess = userGuess.nextInt();
+        while(randomNum != lastGuess){ 
+        
+        if (randomNum < lastGuess){
+        System.out.println("Too High Try Again");
+        
+        
+        }else if (randomNum > lastGuess){
+        System.out.println("Too Low Try Again");
+        
+        }
+        int guess = userGuess.nextInt();
+            if(guess != lastGuess) {
+               guessNum = guessNum + 1;
+               lastGuess = guess;
+            }
+        }
+            System.out.println("Your Right");
+            System.out.println("It took you " + guessNum + " tries");
+        
     }
 
     /**
@@ -25,9 +55,6 @@ public class Main
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
     }
-}
+
